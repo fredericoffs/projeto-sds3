38dd82b2a020c7b81f7b2a16dd82da3bb0e2f797
Frederico F Silva <fredericoffs@live.com>
2021-05-03 19:32:45 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/fredericoffs" target="_blank" rel="noreferrer">Frederico Ferreira</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br />
+                Instagram: <a href="https://instagram.com/fredericosff" target="_blank" rel="noreferrer">@fredericosff</a></small></p>
+            </div>
+        </footer >
+    );
+}
+
+export default Footer;
